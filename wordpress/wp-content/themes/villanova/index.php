03d08db761b5c52fe162b9d5428807c5775dd5c7
Rafal Kuligowski <rafal.kuligowski87@gmail.com>
2026-02-08 13:18:24 +0100

    Page blog - post card

--- wordpress/wp-content/themes/villanova/index.php
+++ wordpress/wp-content/themes/villanova/index.php
@@ -8,7 +8,7 @@
                 <?php
                 $short_description = get_field('short_description', 26);
 
-                if ( !empty( $short_description ) ) : ?>
+                if (!empty( $short_description)) : ?>
                     <p class="banner-description"><?php echo esc_html( $short_description ); ?></p>
                 <?php endif; ?>
 
@@ -17,54 +17,12 @@
         </div>
     </div>
     <div class="page-content">
-        <?php
-            $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-        ?>
-        <?php if($paged == 1): ?>
         <section id="posts">
             <div class="container">
-                <div class="page-title-bar">
-                    <div class="title">Najnowszy artykuł</div>
-                </div>
                 <?php
                 $args = array(
                     'post_type' => 'post',
-                    'posts_per_page' => 1
-                );
-                $the_query = new WP_Query( $args ); ?>
-                <?php if ( $the_query->have_posts() ) : ?>
-                    <div class="posts-wrapper">
-                        <?php $i = 0; ?>
-                        <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                            <a href="<?php echo get_permalink(); ?>" class="post" style="width: 100%; background-image: url('<?php echo get_the_post_thumbnail_url(); ?>');">
-                                <div class="filter"></div>
-                                <div class="title-bar">
-                                    <div class="title"><?php the_title(); ?></div>
-                                    <button class="button clear light">Czytaj więcej</button>
-                                </div>
-                            </a>
-                            <?php $i++; ?>
-                        <?php endwhile; ?>
-                    </div>
-                    <?php wp_reset_postdata(); ?>
-
-                <?php else: ?>
-                    <div class="no-content-info">
-                        Brak aktualności do wyświetlenia. Prosimy wrócić później.
-                    </div>
-                <?php endif; ?>
-            </div>
-        </section>
-        <?php endif; ?>
-        <section id="posts">
-            <div class="container">
-                <div class="page-title-bar">
-                    <div class="title">Wszystkie artykuły</div>
-                </div>
-                <?php
-                $args = array(
-                    'post_type' => 'post',
-                    'posts_per_page' => 10,
+                    'posts_per_page' => 21,
                     'paged'          => get_query_var( 'paged' ),
                 );
                 $the_query = new WP_Query( $args ); ?>
@@ -72,12 +30,16 @@
                     <div class="posts-wrapper">
                         <?php $i = 0; ?>
                         <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                            <a href="<?php echo get_permalink(); ?>" class="post" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>');">
-                                <div class="filter"></div>
-                                <div class="title-bar">
-                                    <div class="title"><?php the_title(); ?></div>
-                                    <button class="button clear light">Czytaj więcej</button>
+                            <a href="<?php echo get_permalink(); ?>" class="post">
+                                <div class="image" style="background-image: url('<?php echo get_the_post_thumbnail_url(); ?>');"></div>
+                                <div class="title"><?php the_title(); ?></div>
+                                <div class="excerpt">
+                                    <?php echo wp_trim_words( get_the_excerpt(), 20, '…' ); ?>
                                 </div>
+                                <div class="date">
+                                    Dodano dnia: <?php echo get_the_date(); ?>
+                                </div>
+                                <button class="button clear dark" style="margin: 15px 0;">Czytaj więcej</button>
                             </a>
                         <?php $i++; ?>
                         <?php endwhile; ?>
